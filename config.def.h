159e6a9ac0d4dcf89850fb6fa9fa84f69942c043
Sander Boer <sanderboer@mauc.nl>
2021-03-10 11:58:58 +0100

    Too lazy to comment

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+//	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 //	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 //	{ MODKEY,                       XK_b,      togglebar,      {0} },
