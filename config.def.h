15a42bee88642ddd9cb9eff0a8c3e3077c33ad55
Sander Boer <sanderboer@mauc.nl>
2022-07-09 12:23:02 +0200

    fullgaps applied

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -193,7 +194,7 @@ static Signal signals[] = {
 	{ "incnmaster",     incnmaster },
 	{ "killclient",     killclient },
 	{ "scratchpad",		  scratchpad },
-	// { "setgaps",        setgaps },
+  { "setgaps",        setgaps },
 	{ "setlayout",      setlayout },
 	{ "setlayoutex",    setlayoutex },
 	{ "setmfact",       setmfact },
