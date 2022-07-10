a34a252a4f339232ac90e0b5cff615d7f3240a0a
Sander Boer <sanderboer@mauc.nl>
2022-07-10 22:17:45 +0200

    actualfullscreen

--- config.def.h
+++ config.def.h
@@ -114,6 +114,7 @@ static Key keys[] = {
 	// { MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
 	// { MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
   //	{ MODKEY,            			XK_x,	   togglescratch,  {.ui = 2 } },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
