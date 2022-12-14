43835b0dc0ab6059534caca9448cd6a29781d543
Sander Boer <sanderboer@mauc.nl>
2022-12-14 21:10:00 +0100

    Too lazy to comment

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static char  dmenufont[]            = "DroidSansMonoNerdFont:size=10:weight=80";
+static const char *fonts[]          = { dmenufont, };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
