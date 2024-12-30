f609b2a9d33332b29a00c30ad4a473bb90768338
Sander Boer <sanderboer@mauc.nl>
2024-12-30 17:35:27 +0100

    ... intermediate commit

--- dwm.c
+++ dwm.c
@@ -32,6 +32,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <X11/cursorfont.h>
+#include <X11/extensions/shape.h>
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
@@ -310,6 +311,7 @@ static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void xrdb(const Arg *arg);
 static void zoom(const Arg *arg);
+void roundcorners(Client *c);
 
 static pid_t getparentprocess(pid_t p);
 static int isdescprocess(pid_t p, pid_t c);
@@ -1501,6 +1503,7 @@ manage(Window w, XWindowAttributes *wa)
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
+  roundcorners(c);
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
@@ -1863,6 +1866,8 @@ restack(Monitor *m)
 	Client *c;
 	XEvent ev;
 	XWindowChanges wc;
+  for (c = m->stack; c; c = c->snext)
+    roundcorners(c);
 
 	drawbar(m);
 	if (!m->sel)
@@ -3118,6 +3123,57 @@ zoom(const Arg *arg)
 			return;
 	pop(c);
 }
+
+void
+roundcorners(Client *c)
+{
+  Window w = c->win;
+  XWindowAttributes wa;
+  XGetWindowAttributes(dpy, w, &wa);
+
+  // If this returns null, the window is invalid.
+  if(!XGetWindowAttributes(dpy, w, &wa))
+    return;
+
+  int width = borderpx * 2 + wa.width;
+  int height = borderpx * 2 + wa.height;
+  /* int width = win_attr.border_width * 2 + win_attr.width; */
+  /* int height = win_attr.border_width * 2 + win_attr.height; */
+  /* int rad = cornerrad * enablegaps * (1-enablefullscreen) * enableoutergaps;  */
+  int rad = cornerrad; 
+//config_theme_cornerradius;
+  int dia = 2 * rad;
+
+  // do not try to round if the window would be smaller than the corners
+  if(width < dia || height < dia)
+    return;
+
+  Pixmap mask = XCreatePixmap(dpy, w, width, height, 1);
+  // if this returns null, the mask is not drawable
+  if(!mask)
+    return;
+
+  XGCValues xgcv;
+  GC shape_gc = XCreateGC(dpy, mask, 0, &xgcv);
+  if(!shape_gc) {
+    XFreePixmap(dpy, mask);
+    return;
+  }
+
+  XSetForeground(dpy, shape_gc, 0);
+  XFillRectangle(dpy, mask, shape_gc, 0, 0, width, height);
+  XSetForeground(dpy, shape_gc, 1);
+  XFillArc(dpy, mask, shape_gc, 0, 0, dia, dia, 0, 23040);
+  XFillArc(dpy, mask, shape_gc, width-dia-1, 0, dia, dia, 0, 23040);
+  XFillArc(dpy, mask, shape_gc, 0, height-dia-1, dia, dia, 0, 23040);
+  XFillArc(dpy, mask, shape_gc, width-dia-1, height-dia-1, dia, dia, 0, 23040);
+  XFillRectangle(dpy, mask, shape_gc, rad, 0, width-dia, height);
+  XFillRectangle(dpy, mask, shape_gc, 0, rad, width, height-dia);
+  XShapeCombineMask(dpy, w, ShapeBounding, 0-wa.border_width, 0-wa.border_width, mask, ShapeSet);
+  XFreePixmap(dpy, mask);
+  XFreeGC(dpy, shape_gc);
+}
+ 
 
 int
 main(int argc, char *argv[])
